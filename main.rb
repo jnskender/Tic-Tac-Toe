27e23bcb802517904df8fc135a702ee52ed2c85f
John Skender <jnskender@gmail.com>
2017-02-24 22:05:35 -0600

    Pseudocode Expectations

---
+++ main.rb
@@ -0,0 +1,13 @@
+#Player one name?
+#Player two name?
+#Welcome to Tic Tac Toe!
+#Please enter a the number that represents where you want to play your piece!
+#Player 1, you are X's. Please Choose a cell.
+#Validate choice
+#Update Board
+#Player 2, you are 0's. Please Choose a cell.
+#Validate choice
+#After 3 move check for winners after each turn
+#If winner Display Player(?) Wins!
+#On End game
+#Start new game?
